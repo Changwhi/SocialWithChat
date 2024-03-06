dd228317c9b8f52b2833d49230c6389647907459
Changwhi <whinada@gmail.com>
2024-03-06 10:47:59 -0800

    feat: setup middleware

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,10 +1,20 @@
 import  express  from "express"
 import dotenv from "dotenv";
+import connectDB from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
+connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // To parse JSON data in the req.body
+app.use(express.urlencoded({extended: true})); // To parse object or nasted object the req.body
+// app.use(express.urlencoded({extended: false})); TO parse string or arrays.
+app.use(cookieParser()); // get cookie from req set cookie inside req
+
+app.use("api/users", userRoutes)
 
 app.listen(5000, () => console.log(`Server started at http://localhost:${PORT}`));
 
