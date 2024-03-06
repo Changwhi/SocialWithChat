d3d7d232fdd6105b18d55386cece87f1e1afb5d4
Changwhi <whinada@gmail.com>
2024-03-06 10:52:36 -0800

    fix: remove reprecated options

--- Back-End/db/connectDB.js
+++ Back-End/db/connectDB.js
@@ -4,10 +4,7 @@ const connectDB = async () => {
 	try {
 		const conn = await mongoose.connect(process.env.MONGO, {
 			// To avoid warnings in the console
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
 		});
-
 		console.log(`MongoDB Connected: ${conn.connection.host}`);
 	} catch (error) {
 		console.error(`Error: ${error.message}`);
