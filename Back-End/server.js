1757fdc2a59375cde53df44789025ada24b950f1
Changwhi <whinada@gmail.com>
2024-03-11 11:03:42 -0700

    feat: install and setup cloudinary

--- Back-End/server.js
+++ Back-End/server.js
@@ -4,12 +4,19 @@ import connectDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js"
 import postRoutes from "./routes/postRouter.js"
+import {v2 as cloudinary} from "cloudinary";
 
 dotenv.config();
 connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API,
+    api_secret: process.env.CLOUDINARY_SECRET,
+})
 
 app.use(express.json()); // To parse JSON data in the req.body
 app.use(express.urlencoded({extended: true})); // To parse object or nasted object the req.body
