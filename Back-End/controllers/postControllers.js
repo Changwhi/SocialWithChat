312487fcb76ec4986559cabf74987aabc6781e5e
Changwhi <whinada@gmail.com>
2024-03-12 17:42:57 -0700

    feat: add a function that allows users to upload picture in their post

--- Back-End/controllers/postControllers.js
+++ Back-End/controllers/postControllers.js
@@ -1,36 +1,41 @@
 import User from "../models/userModel.js";
 import Post from "../models/PostModel.js";
+import { v2 as cloudinary } from "cloudinary";
+
 const createPost = async (req, res) => {
   try {
-    const { postedBy, text, img } = req.body;
-
+    const { postedBy, text } = req.body;
+    let {img} = req.body;
     if (!postedBy || !text) {
       return res
         .status(400)
-        .json({ message: "Postedby and text fileds are required." });
+        .json({ error: "Postedby and text fileds are required." });
     }
-
+    if (img) {
+        const uploadedResponse = await cloudinary.uploader.upload(img);
+        img = await uploadedResponse.secure_url;
+    }
     const user = await User.findById(postedBy);
 
-    if (!user) return res.status(400).json({ message: "User are not found" });
+    if (!user) return res.status(400).json({ error: "User are not found" });
 
     if (user._id.toString() !== req.user._id.toString()) {
-      return res.status(401).json({ message: "Unauthorized to create post" });
+      return res.status(401).json({ error: "Unauthorized to create post" });
     }
 
     const maxLength = 500;
     if (text.length > maxLength) {
       return res
         .status(400)
-        .json({ message: `Text must be less than ${maxLength} characters` });
+        .json({ error: `Text must be less than ${maxLength} characters` });
     }
 
     const newPost = new Post({ postedBy, text, img });
     await newPost.save();
     res.status(201).json({ message: "Post created successfully", newPost });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-    console.log("Error in createPost: ", error.message);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.log("Error in createPost: ", err.message);
   }
 };
 
@@ -39,12 +44,12 @@ const getPost = async (req, res) => {
     const post = await Post.findById(req.params.id);
 
     if (!post) {
-      return res.status(404).json({ message: "Post not found" });
+      return res.status(404).json({ error: "Post not found" });
     }
     res.status(200).json(post);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-    console.log("Error in getPost: ", error.message);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+    console.log("Error in getPost: ", err.message);
   }
 };
 
@@ -118,25 +123,26 @@ const replyToPost = async (req, res) => {
     post.replies.push(reply);
     await post.save();
     res.status(200).json({ message: "Reply to a post successfilly" });
-
   } catch (err) {
     res.status(500).json({ error: err.message });
     console.log("Error in replyToPost: ", err.message);
   }
 };
 
-const getFeedPosts = async(req,res) => {
-    try {
-       const userId = req.user._id;
-       const user = await User.findById(userId) ;
-       if(!user) return res.status(404).json({error: "User can not found"})
-       const following = user.following;
+const getFeedPosts = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const user = await User.findById(userId);
+    if (!user) return res.status(404).json({ error: "User can not found" });
+    const following = user.following;
 
-       const feedPosts = await Post.find({postedBy:{$in:following}}).sort({createdAt: -1});
-       res.status(200).json({feedPosts})
-    } catch (err) {
+    const feedPosts = await Post.find({ postedBy: { $in: following } }).sort({
+      createdAt: -1,
+    });
+    res.status(200).json({ feedPosts });
+  } catch (err) {
     res.status(500).json({ error: err.message });
     console.log("Error in getFeedPosts: ", err.message);
-    }
-}
-export { createPost, getPost, deletePost, likePost, replyToPost, getFeedPosts};
+  }
+};
+export { createPost, getPost, deletePost, likePost, replyToPost, getFeedPosts };
