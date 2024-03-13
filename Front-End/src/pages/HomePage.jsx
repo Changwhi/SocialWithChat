8096e5c8c222fec4b75b132a6abe2319040706c3
Changwhi <whinada@gmail.com>
2024-03-13 01:41:22 -0700

    feat: implement homepage with post component to show all feed

--- Front-End/src/pages/HomePage.jsx
+++ Front-End/src/pages/HomePage.jsx
@@ -1,12 +1,44 @@
-import { Button } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import {  Flex, Spinner } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import useShowToast from "../hooks/useShowToast";
+import Post from "../components/Post";
 
 const HomePage = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const showToast = useShowToast();
+
+  useEffect(() => {
+    const getFeedPosts = async () => {
+      try {
+        const response = await fetch("/api/posts/feed");
+        const responseData = await response.json();
+        if (!responseData) {
+          showToast("Error", responseData.error, "error");
+          return;
+        }
+        setPosts(responseData);
+      } catch (error) {
+        showToast("Error", error.message, "error");
+      } finally {
+        setLoading(false);
+      }
+    };
+    getFeedPosts();
+  }, [showToast]);
   return (
     <>
-      <Link to={"/changwhi"}>
-        <Button> GO to </Button>
-      </Link>
+      {!loading && posts.length === 0 && <h1>No posts can be found.</h1>}
+      {loading && (
+        <Flex justify={"center"}>
+          <Spinner size={"xl"} />
+        </Flex>
+      )}
+
+      {posts.map((post)=>(
+          <Post key={post._id} post={post} postedBy={post.postedBy} />
+      ))}
     </>
   );
 };
