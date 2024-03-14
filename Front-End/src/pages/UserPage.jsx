d057bea50977240b25858dffb194ce3c7ff03b2d
Changwhi <whinada@gmail.com>
2024-03-14 16:19:08 -0700

    refactor: use global state for posts

--- Front-End/src/pages/UserPage.jsx
+++ Front-End/src/pages/UserPage.jsx
@@ -5,12 +5,14 @@ import useShowToast from "../hooks/useShowToast";
 import { Flex, Spinner } from "@chakra-ui/react";
 import Post from "../components/Post";
 import useGetUserProfile from "../hooks/useGetUserProfile";
+import { useRecoilState} from "recoil";
+import postsAtom from "../components/atoms/postsAtom";
 
 const UserPage = () => {
   const {user,loading} = useGetUserProfile();
   const { username } = useParams();
   const showToast = useShowToast();
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useRecoilState(postsAtom);
   const [fetchingPosts, setFetchingPosts] = useState(true);
 
   useEffect(() => {
@@ -32,7 +34,7 @@ const UserPage = () => {
     };
 
     getPosts();
-  }, [username, showToast]);
+  }, [username, showToast, setPosts]);
 
 
   if (!user && loading) {
