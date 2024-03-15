6ab5af59c0424027f750aaad84384db9fdabfc8f
Changwhi <whinada@gmail.com>
2024-03-15 11:14:03 -0700

    feat: create draft chat page

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -7,9 +7,9 @@ import HomePage from "./pages/HomePage"
 import AuthPage from "./pages/AuthPage"
 import { useRecoilValue } from "recoil"
 import userAtom from "./components/atoms/userAtom"
-import LogoutButton from "./components/auth/LogoutButton"
 import UpdateProfilePage from "./pages/UpdateProfilePage"
 import CreatePost from "./components/CreatePost"
+import ChatPage from "./pages/ChatPage"
 
 function App() {
   const user = useRecoilValue(userAtom);
@@ -21,11 +21,11 @@ function App() {
         <Route path="/" element={user? <HomePage /> : <Navigate to='/auth' />}></Route>
         <Route path="/auth" element={!user ? <AuthPage />: <Navigate to='/'/>}></Route>
         <Route path="/update" element={user ? <UpdateProfilePage/>: <Navigate to='/auth'/>}></Route>
-        <Route path="/:username" element={<UserPage />}></Route>
+        <Route path="/:username" element={user ? (<><UserPage/> <CreatePost/></>) : (<UserPage />)}></Route>
         <Route path="/:username/post/:pid" element={<PostPage/>}></Route>
+        <Route path="/chat" element={user ? <ChatPage /> : <Navigate to={'/auth'} />}></Route>
       </Routes>
-      {user && <LogoutButton />} 
-      {user && <CreatePost/>} 
+
     </Container>
   )
 }
