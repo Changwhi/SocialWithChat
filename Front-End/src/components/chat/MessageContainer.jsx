9bbcb3786898b679ba79433af9fcc3ff6b67c651
Changwhi <whinada@gmail.com>
2024-03-18 11:43:42 -0700

    fix: correct component name

--- Front-End/src/components/chat/MessageContainer.jsx
+++ Front-End/src/components/chat/MessageContainer.jsx
@@ -8,7 +8,7 @@ import {
   Text,
   useColorModeValue,
 } from "@chakra-ui/react";
-import Message from "./Mmessage";
+import Message from "./Message";
 import MessageInput from "./MessageInput";
 
 const MessageContainer = () => {
