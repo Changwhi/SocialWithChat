4319e99f743ad924f32b97a18f650502a58d1307
Changwhi <whinada@gmail.com>
2024-04-05 23:38:27 -0700

    feat: add seen property to check read or not

--- Back-End/models/conversationModel.js
+++ Back-End/models/conversationModel.js
@@ -1,18 +1,27 @@
 import mongoose from "mongoose";
 
-
-const conversationSchema = mongoose.Schema({
-  participants: [{
-     type:mongoose.Schema.Types.ObjectId, ref: 'User' 
-  }],
-  lastMessage:{
-    text: String,
-    sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+const conversationSchema = mongoose.Schema(
+  {
+    participants: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    lastMessage: {
+      text: String,
+      sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+      seen: {
+        type: Boolean,
+        default: false,
+      },
+    },
   },
-}, {
-    timestamps: true
-});
+  {
+    timestamps: true,
+  }
+);
 
-const Conversation = mongoose.model('Conversation', conversationSchema);
+const Conversation = mongoose.model("Conversation", conversationSchema);
 
 export default Conversation;
