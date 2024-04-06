bcb7371b765482beef5a5cfd65101b3987b27832
Changwhi <whinada@gmail.com>
2024-04-05 23:44:39 -0700

    feat: update last message

--- Front-End/src/components/chat/MessageInput.jsx
+++ Front-End/src/components/chat/MessageInput.jsx
@@ -2,19 +2,24 @@ import { Input, InputGroup, InputRightElement } from "@chakra-ui/react";
 import { useState } from "react";
 import { IoSendSharp } from "react-icons/io5";
 import useShowToast from "../../hooks/useShowToast";
-import { useRecoilValue } from "recoil";
-import { selectedConversationAtom } from "../atoms/messagesAtom";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import {
+  conversationsAtom,
+  selectedConversationAtom,
+} from "../atoms/messagesAtom";
 
-const MessageInput = ({ setMessage }) => {
+const MessageInput = ({ setMessages }) => {
   const [text, setText] = useState("");
   const selectedConversation = useRecoilValue(selectedConversationAtom);
+  const setConversations = useSetRecoilState(conversationsAtom);
+
   const showToast = useShowToast();
   const sendingHanlder = async (e) => {
     e.preventDefault();
     if (!text) return;
 
     try {
-      const response = fetch("/api/messages", {
+      const response = await fetch("/api/messages", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -31,8 +36,23 @@ const MessageInput = ({ setMessage }) => {
         showToast("Error", responseData.error, "error");
         return;
       }
-
-      setMessage((messages) => [...messages, responseData]);
+      setText("");
+      setMessages((messages) => [...messages, responseData]);
+      setConversations((prevConversations) => {
+        const updatedConversation = prevConversations.map((conversation) => {
+          if (conversation._id === selectedConversation._id) {
+            return {
+              ...conversation,
+              lastMessage: {
+                text: text,
+                sender: responseData.sender,
+              },
+            };
+          }
+          return conversation;
+        });
+        return updatedConversation;
+      });
     } catch (error) {
       showToast("Error", error.message, "error");
     }
