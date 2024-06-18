9e7fee2443688e3f77f50090c34d8f392b974007
Changwhi <whinada@gmail.com>
2024-06-18 11:09:43 -0700

    feat: add new route for suggesting other users

--- Back-End/routes/userRoutes.js
+++ Back-End/routes/userRoutes.js
@@ -1,15 +1,24 @@
-import express from 'express'
-import {loginUser, logoutUser, singUpUser, followAndUnfollow, updateUser, getProfile} from '../controllers/userControllers.js'
-import protectRoute from '../middlewares/protectRoute.js';
+import express from "express";
+import {
+  getSuggestedUsers,
+  loginUser,
+  logoutUser,
+  singUpUser,
+  followAndUnfollow,
+  updateUser,
+  getProfile,
+} from "../controllers/userControllers.js";
+import protectRoute from "../middlewares/protectRoute.js";
+import useGetUserProfile from "../../Front-End/src/hooks/useGetUserProfile.js";
 
 const router = express.Router();
 
-router.get("/profile/:userinfo", getProfile)
+router.get("/profile/:userinfo", getProfile);
+router.get("/suggested", protectRoute, getSuggestedUsers);
 router.post("/signup", singUpUser);
 router.post("/login", loginUser);
-router.post("/logout", logoutUser)
+router.post("/logout", logoutUser);
 router.post("/follow/:id", protectRoute, followAndUnfollow);
-router.put("/update/:id", protectRoute, updateUser)
-
+router.put("/update/:id", protectRoute, updateUser);
 
 export default router;
