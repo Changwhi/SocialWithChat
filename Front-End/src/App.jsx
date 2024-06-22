5a404bb26f974dd709e324fb84682360c1b87c54
Changwhi <whinada@gmail.com>
2024-06-22 02:02:58 -0700

    design: adjust width for better UI

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -13,19 +13,17 @@ import ChatPage from "./pages/ChatPage"
 
 function App() {
   const user = useRecoilValue(userAtom);
-  const {pathname} = useLocation();
-
 
   return (
     <Box position={"relative"} w={"full"}>
 
-    <Container maxW={pathname === '/' ? {base:"620px", md:"960px"}: "640px"}>
+    <Container maxW={{base:"620px", md:"960px"}}>
       <Header />
       <Routes>
         <Route path="/" element={user? <HomePage /> : <Navigate to='/auth' />}></Route>
         <Route path="/auth" element={!user ? <AuthPage />: <Navigate to='/'/>}></Route>
         <Route path="/update" element={user ? <UpdateProfilePage/>: <Navigate to='/auth'/>}></Route>
-        <Route path="/:username" element={user ? (<><UserPage/> <CreatePost/></>) : (<UserPage />)}></Route>
+        <Route path="/:username" element={<UserPage />}></Route>
         <Route path="/:username/post/:pid" element={<PostPage/>}></Route>
         <Route path="/chat" element={user ? <ChatPage /> : <Navigate to={'/auth'} />}></Route>
       </Routes>
