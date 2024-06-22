099914f027a2f26e85d24dbb35aafe7bb55a58ca
Changwhi <whinada@gmail.com>
2024-06-22 02:04:01 -0700

    design: modify icons

--- Front-End/src/components/Header.jsx
+++ Front-End/src/components/Header.jsx
@@ -1,13 +1,15 @@
-import { Button, Flex, Image, Link, useColorMode } from "@chakra-ui/react";
-import {useRecoilValue, useSetRecoilState } from "recoil";
+import { Flex, Link, Text, useColorMode } from "@chakra-ui/react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import userAtom from "./atoms/userAtom";
-import { AiFillHome } from "react-icons/ai";
 import { RxAvatar } from "react-icons/rx";
-import { BsFillChatQuoteFill} from "react-icons/bs";
+import { BsFillChatQuoteFill } from "react-icons/bs";
 import { Link as RouterLink } from "react-router-dom";
 import { FiLogOut } from "react-icons/fi";
 import useLogout from "../hooks/useLogout";
 import authScreenAtom from "./atoms/authAtom";
+import { GrSun } from "react-icons/gr";
+import { IoMoonSharp } from "react-icons/io5";
+import { AiFillHome } from "react-icons/ai";
 
 const Header = () => {
   const { colorMode, toggleColorMode } = useColorMode();
@@ -18,44 +20,92 @@ const Header = () => {
     <>
       <Flex justifyContent={"space-between"} mt={6} mb={12}>
         {user && (
-          <Link as={RouterLink} to={"/"}>
-            <AiFillHome size={24} />
+          <Link cursor={"pointer"} as={RouterLink} to={"/"}>
+            <Text fontWeight={"bold"} fontSize={{ base: "large", md: "2xl" }}>
+              Post & Chat
+            </Text>
           </Link>
         )}
-   {!user && (
-          <Link as={RouterLink} to={'/auth'} onClick={
-            () => setAuthScreen("login")
-
-          }>
-          Login
+        {!user && (
+          <Link
+            as={RouterLink}
+            to={"/auth"}
+            onClick={() => setAuthScreen("login")}
+            fontSize={"xl"}
+          >
+            Login
           </Link>
         )}
-        <Image
-          cursor={"pointer"}
-          alt="logo"
-          w={12}
-          src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
-          onClick={toggleColorMode}
-        />
         {user && (
-          <Flex alignItems={"center"} gap={4}>
+          <Flex alignItems={"center"} gap={{ base: 3, md: 6 }}>
+            <Link cursor={"pointer"} as={RouterLink} to={"/"}>
+              <Flex alignItems={"center"} gap={1}>
+                <AiFillHome size={24} />
+                <Text display={{ base: "none", md: "block" }} fontSize={"sm"}>
+                  Home
+                </Text>
+              </Flex>
+            </Link>
             <Link as={RouterLink} to={`/${user.username}`}>
-              <RxAvatar size={24} />
+              <Flex alignItems={"center"} gap={1}>
+                <RxAvatar size={24} />
+                <Text display={{ base: "none", md: "block" }} fontSize={"sm"}>
+                  My Page
+                </Text>
+              </Flex>
             </Link>
             <Link as={RouterLink} to={`/chat`}>
-              <BsFillChatQuoteFill size={24} />
+              <Flex alignItems={"center"} gap={1}>
+                <BsFillChatQuoteFill size={24} />
+                <Text display={{ base: "none", md: "block" }} fontSize={"sm"}>
+                  Chat
+                </Text>
+              </Flex>
             </Link>
-            <Button size={"xs"} onClick={logout}>
-              <FiLogOut size={20} />
-            </Button>
+            <Link
+              alignItems={"center"}
+              cursor={"pointer"}
+              gap={1}
+              onClick={toggleColorMode}
+            >
+              <Flex alignItems={"center"} gap={1}>
+                {colorMode === "dark" ? (
+                  <GrSun size={24} />
+                ) : (
+                  <IoMoonSharp size={24} />
+                )}
+                {colorMode === "dark" ? (
+                  <Text display={{ base: "none", md: "block" }} fontSize={"sm"}>
+                    Light Mode
+                  </Text>
+                ) : (
+                  <Text display={{ base: "none", md: "block" }} fontSize={"sm"}>
+                    Dark Mode
+                  </Text>
+                )}
+              </Flex>
+            </Link>
+            <Link size={"xs"} onClick={logout}>
+              <Flex alignItems={"center"} gap={1}>
+                <FiLogOut size={24} />
+                <Text display={{ base: "none", md: "block" }} fontSize={"sm"}>
+                  Logout
+                </Text>
+              </Flex>
+            </Link>
           </Flex>
         )}
-      
-   {!user && (
-          <Link as={RouterLink} to={'/auth'} onClick={
-            () => {setAuthScreen("signup")}
-          }>
-           Sign up
+
+        {!user && (
+          <Link
+            as={RouterLink}
+            to={"/auth"}
+            onClick={() => {
+              setAuthScreen("signup");
+            }}
+            fontSize={"xl"}
+          >
+            Sign up
           </Link>
         )}
       </Flex>
