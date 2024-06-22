7148468c243a57e3c534cc09beece73825b1b7fb
Changwhi <whinada@gmail.com>
2024-06-22 02:05:54 -0700

    design: add post button in this component for better UX

--- Front-End/src/components/UserHeader.jsx
+++ Front-End/src/components/UserHeader.jsx
@@ -11,27 +11,27 @@ import {
   MenuList,
   Menu,
   Button,
+  useColorModeValue,
 } from "@chakra-ui/react";
-
-import { BsInstagram } from "react-icons/bs";
 import { CgMoreO } from "react-icons/cg";
 import { useRecoilValue } from "recoil";
 import userAtom from "./atoms/userAtom";
 import { Link as RouterLink } from "react-router-dom";
 import useShowToast from "../hooks/useShowToast";
 import useFollowAndUnFollow from "../hooks/useFollowAndUnFollow";
+import CreatePost from "./CreatePost";
 
 const UserHeader = ({ user }) => {
   const SUCCESS = "Success.";
   const COPIED = "This link copied";
-  const INTSTAGRAM = "Instagram.com";
   const COPYLINK = "Copy link";
   const THREADS = "Threads";
-  const REPLIES = "Replies";
+  const FOLLOWERS = "Followers";
 
   const showToast = useShowToast();
   const currentUser = useRecoilValue(userAtom); // this is the logged in user
   const { followHandler, updating, following } = useFollowAndUnFollow(user);
+  const buttonColor = useColorModeValue("gray.300", "gray.dark");
 
   const copyURL = () => {
     const currentURL = window.location.href;
@@ -42,54 +42,61 @@ const UserHeader = ({ user }) => {
 
   return (
     <>
-      <VStack gap={4} alignItems={"start"}>
+      <VStack pb={10} gap={4} alignItems={"start"}>
         <Flex justifyContent={"space-between"} w={"full"}>
           <Box>
             <Text fontSize={"2xl"} fontWeight={"bold"}>
-              {user.name}
+              Hi👋, {user.name}
             </Text>
             <Flex gap={2} alignItems={"center"}>
-              <Text fontSize={"sm"}>{user.username}</Text>
               <Text
-                fontSize={"xs"}
+                fontSize={"md"}
                 bg={"gray.dark"}
                 color={"gray.light"}
-                p={1}
+                p={2}
                 borderRadius={"full"}
               >
-                Threads.next
+                {user.username}
               </Text>
             </Flex>
           </Box>
           <Box>
-            {user.profilePic && (
-              <Avatar
-                name={user.name}
-                src={user.profilePic}
-                size={{
-                  base: "md",
-                  md: "xl",
-                }}
-              />
-            )}
-            {!user.profilePic && (
-              <Avatar
-                name=" "
-                src="https://bit.ly/broken-link"
-                size={{
-                  base: "md",
-                  md: "xl",
-                }}
-              />
-            )}
+            <Flex alignItems={"center"} flexDirection={"column"}>
+              {user.profilePic && (
+                <Avatar
+                  name={user.name}
+                  src={user.profilePic}
+                  size={{
+                    base: "md",
+                    md: "xl",
+                  }}
+                />
+              )}
+              {!user.profilePic && (
+                <Avatar
+                  name=" "
+                  src="https://bit.ly/broken-link"
+                  size={{
+                    base: "md",
+                    md: "xl",
+                  }}
+                  bg={"gray.300"}
+                />
+              )}
+            </Flex>
           </Box>
         </Flex>
         <Text>{user.bio}</Text>
-        {currentUser?._id === user._id && (
-          <Link as={RouterLink} to="/update">
-            <Button size={"sm"}>Update Profile</Button>
-          </Link>
-        )}
+        <Flex alignContent={"space-between"} w={"full"} gap={4}>
+          {currentUser?._id === user._id && <CreatePost />}
+          {currentUser?._id === user._id && (
+            <Link as={RouterLink} to="/update">
+              <Button bg={buttonColor} size={{ base: "sm", sm: "md" }}>
+                Update Profile
+              </Button>
+            </Link>
+          )}
+        </Flex>
         {currentUser?._id !== user._id && (
           <Button onClick={followHandler} size={"sm"} isLoading={updating}>
             {following ? "Unfollow" : "Follow"}
@@ -97,14 +104,11 @@ const UserHeader = ({ user }) => {
         )}
         <Flex w={"full"} justifyContent={"space-between"}>
           <Flex gap={2} alignItems={"center"}>
-            <Text color={"gray.light"}>{user.followers.length} followers</Text>
-            <Box w={"1"} h={"1"} bg={"gray.light"} borderRadius={"full"}></Box>
-            <Link color={"gray.light"}>{INTSTAGRAM}</Link>
+            <Text color={"gray.light"}>
+              {user.followers.length} {FOLLOWERS}
+            </Text>
           </Flex>
           <Flex>
-            <Box className="icon-container">
-              <BsInstagram size={24} cursor={"pointer"} />
-            </Box>
             <Box className="icon-container">
               <Menu>
                 <MenuButton>
@@ -130,16 +134,6 @@ const UserHeader = ({ user }) => {
             cursor={"pointer"}
           >
             <Text fontWeight={"bold"}>{THREADS}</Text>
-          </Flex>
-          <Flex
-            flex={1}
-            borderBottom={"1.5px solid gray"}
-            justifyContent={"center"}
-            color={"gray.light"}
-            pb="3"
-            cursor={"pointer"}
-          >
-            <Text fontWeight={"bold"}>{REPLIES}</Text>
           </Flex>
         </Flex>
       </VStack>
